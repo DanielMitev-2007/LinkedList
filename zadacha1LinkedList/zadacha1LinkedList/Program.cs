8bb7fa3eaea3c662fbc6879d235500ea6e62ddd2
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-03-16 13:06:55 +0200

    Add files via upload

---
+++ zadacha1LinkedList/zadacha1LinkedList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+            int[] values = { 42, 7, 19, 3, 88, 56 };
+            Array.Sort(values);
+            foreach (int num in values)
+            {
+                numbers.AddLast(num);
+            }
+
+            Console.WriteLine("Оригинален списък:");
+            Console.WriteLine(string.Join(" -> ", numbers) + " -> null");
+
+            SearchNumber(numbers, 19);
+            SearchNumber(numbers, 100);
+
+            LinkedList<int> reversedList = ReverseLinkedList(numbers);
+            Console.WriteLine("Обърнат списък.");
+            Console.WriteLine(string.Join(" -> ", reversedList) + " -> null");
+
+            Console.ReadKey();
+        }
+
+        static void SearchNumber(LinkedList<int> list, int num)
+        {
+            if (list.Contains(num))
+            {
+                Console.WriteLine($"Числото {num} е в списъка.");
+            }
+            else
+            {
+                Console.WriteLine($"Числото {num} не е в списъка.");
+            }
+        }
+
+        static LinkedList<int> ReverseLinkedList(LinkedList<int> list)
+        {
+            LinkedList<int> reversedList = new LinkedList<int>();
+            foreach (int num in list)
+            {
+                reversedList.AddFirst(num);
+            }
+            return reversedList;
+        }
+    }
+}
